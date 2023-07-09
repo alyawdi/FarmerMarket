0e81defb5bed74da0be2993725ea29af4d10847d
10121668@mu.edu.lb <10121668@mu.edu.lb>
2023-07-09 17:02:08 +0300

    Add project files.

---
+++ AliAwdiAnotherOne/Controllers/FarmerMarketControll.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AliAwdiAnotherOne.Models;
+using System.Xml.Linq;
+
+namespace AliAwdiAnotherOne.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FarmerMarketControll : ControllerBase
+
+    {
+        private readonly ILogger<FarmerMarketControll> _logger;
+
+        public FarmerMarketControll(ILogger<FarmerMarketControll> logger)
+        {
+            _logger = logger;
+        }
+        private static readonly List<FarmerMarket> farmermarket = new();
+
+        [HttpGet]
+        public IEnumerable<FarmerMarket> Get()
+        {
+            _logger.LogInformation("You fetched all the goods in your store");
+            return farmermarket;
+        }
+
+        [HttpGet("id")]
+        public FarmerMarket GetById(int id)
+        {
+            _logger.LogInformation($"you fetch the vegetables/fruits with the following {id}");
+            return farmermarket.First(farmermarket => farmermarket.Id == id);
+        }
+
+        [HttpPost]
+        public FarmerMarket Create(string name, int quantity)
+        {
+            FarmerMarket farmerMarket = new(name, quantity);
+            farmermarket.Add(farmerMarket);
+            return farmerMarket;
+        }
+
+        [HttpPut]
+        public FarmerMarket Update(int searchingid, string name, int quantity)
+        {
+            var farmerMarket = farmermarket.Where(farmerMarket => farmerMarket.Id == searchingid).FirstOrDefault();
+
+            farmerMarket.UpdateFramework(name, quantity);
+            return farmerMarket;
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            farmermarket.RemoveAll(farmermaket => farmermaket.Id == id);
+
+
+        }
+        [HttpPatch]
+        public FarmerMarket Patch(int id, int quantity)
+        {
+            var farmerMarket = farmermarket.Where(farmerMarket => farmerMarket.Id == id).FirstOrDefault();
+
+            farmerMarket.UpdateFramework(farmerMarket.Name, quantity);
+            return farmerMarket;
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ AliAwdiAnotherOne/Models/FarmerMarket.cs
@@ -0,0 +1,24 @@
+﻿namespace AliAwdiAnotherOne.Models
+{
+    public class FarmerMarket
+    {
+        public int Id { get; private set; }
+        private static int counter=1;
+        public string Name { get; private set; }
+        public int Quantity { get; private set; }
+        public FarmerMarket( string name, int quantity)
+        {
+            Id = counter++;
+            Name = name;
+            Quantity = quantity < 0 ? throw new Exception("the quantity can't be less than zero") : quantity;
+
+        }
+        public void UpdateFramework(  string newname, int newquantity)
+        {  Name = newname;
+           Quantity = newquantity <=0 ? throw new Exception ("how dare u put quantity below than zero") : newquantity;
+        }
+        
+
+    }
+}
+
